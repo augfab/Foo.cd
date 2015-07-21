0e1ab3b4c388002b2ab1c023f8d869177783dc58
Augustin Fabre <augustin.fabre@telecom-bretagne.eu>
2015-07-21 19:37:55 +0200

    remote: refactor the code, more classic help and controls
    Stop with Backspace, quit with ^D (standard for programs which read
    input from the terminal).
    Invert song and volume control, it seems more natural.
    AZERTY and QWERTY.
    
    Add some options.
    * Standard help message.
    * Can specify a pipe to write to.
    
    Perform some validity tests on command-line arguments and environment,
    exit cleanly.
    No more error output when a wrong key is pressed in the terminal, it
    was an annoyance.
    
    Remove the hack with $i < 1 to break the loop.
    
    Be careful with the UTF-8 BOM (first two bytes of the file, U+FEFF) !
    It messed up the shebang, bash was complaining about that first line.

--- remote.sh
+++ remote.sh
@@ -1,59 +1,132 @@
-﻿#!/bin/bash
+#!/bin/bash
 
-i="0"
+print_help() {
+	cat <<-EOF
+	Usage: $(basename $0) [-h] [-p PIPE]
+	Control Foo.cd via this remote interface
 
-# Absolute path this script is in
-SCRIPTPATH=$(dirname $(readlink -f "$0"))
-PIPE="pipe"
+	Options:
+	    -h               Print this help and exit
+	    -p PIPE          Path to a writable named pipe, defaults to ./pipe
 
-echo -e \\033c
-	
-	echo -e '\e[91m	  -=+/  Foo.cd pipe commands  \+=- \e[39m'
-	echo ""
-	echo -e "\e[37m	Play/Pause       Stop         Quit \e[21m"
-	echo -e "\e[1m	    1             2            9 \e[21m"
-	echo ""
-	echo -e "\e[37m	 Navigate       Volume      Previous/"
-	echo -e "	 in Tree        Up/Down     Next Song \e[39m"
-	echo -e '\e[1m	    Z                          ↑'
-	echo -e '	  Q S D          ←  →          ↓ \e[21m'
-	echo ""
-	echo ""
-	echo -e "\e[37m	 Validate       Append  \e[21m"
-	echo -e "\e[1m	  Enter          TBD \e[21m"
-	echo ""
-	echo ""
-	
-while [ $i -lt 1 ]
-do	
+	Write following commands to the terminal in order to control Foo.cd:
+	    Space, 1         Play/Pause
+	    BackSpace, 2     Stop
+	    CTRL-D,9         Quit
+	    W, A, S, D       Navigate in tree
+	    Up, Down         Volume Up/Down
+	    Right, Left      Previous/Next song
+	    Enter            Play selected song
+	    Tab              Append selected song to playlist
+
+	Exit status:
+	    Returns 1 if no writable pipe (default ./pipe or inputted with -p) \
+exists.
+	EOF
+}
+
+while [ $# -gt 0 ]
+do
+	key="$1"
+	case $key in
+		-h)
+			print_help
+			exit 0
+			;;
+		-p)
+			shift
+			if [ -p "$1" ]
+			then
+				if [ -w "$1" ]
+				then
+					$PIPE="$1"
+				else
+					>&2 echo "$1 does not have write permission"
+					>&2 print_help
+					exit 1
+				fi
+			else
+				>&2 echo "$1 is not a pipe or does not exist"
+				>&2 print_help
+				exit 1
+			fi
+			;;
+		*)
+			;;
+	esac
+	shift
+done
+
+if [ -z "$PIPE" ]
+then
+	SCRIPTPATH=$(dirname $(readlink -f $0))
+	if [ -p "$SCRIPTPATH/pipe" ] && [ -w "$SCRIPTPATH/pipe" ]
+	then
+		PIPE="$SCRIPTPATH/pipe"
+	else
+		>&2 echo "$SCRIPTPATH/pipe is not a writable pipe or does not exist"
+		>&2 print_help
+		exit 1
+	fi
+fi
+
+
+# internal field separator for read
+# enables the use of space and tab as normal input characters
+IFS=""
+
+while true
+do
 	read -rsn1 ui
 	case "$ui" in
-		$'\x1b')    # Handle ESC sequence.
-		# Flush read. We account for sequences for Fx keys as
-		# well. 6 should suffice far more then enough.
-		read -rsn1 -t 0.01 tmp
-		if [[ "$tmp" == "[" ]]; then
+		$'\033') # escape for ^[ Escape key
 			read -rsn1 -t 0.01 tmp
-			case "$tmp" in
-				"A") printf "Up\n";;
-				"B") printf "Down\n";;
-				"C") echo "volume_up" > $SCRIPTPATH/$PIPE;;
-				"D") echo "volume_down" > $SCRIPTPATH/$PIPE;;
-			esac
-		fi
-		# Flush "stdin" with 0.1  sec timeout.
-		read -rsn5 -t 0.01;;
-		
-		# Other one byte (char) cases. Here only quit.
-		"") echo "tree_validate" > $SCRIPTPATH/$PIPE;;
-		z) echo "tree_up" > $SCRIPTPATH/$PIPE;;
-		s) echo "tree_down" > $SCRIPTPATH/$PIPE;;
-		q) echo "tree_left" > $SCRIPTPATH/$PIPE;;
-		d) echo "tree_right" > $SCRIPTPATH/$PIPE;;
-		1) echo "play_pause" > $SCRIPTPATH/$PIPE;;
-		2) echo "stop" > $SCRIPTPATH/$PIPE;;
-		9) echo "quit" > $SCRIPTPATH/$PIPE; i=1 ;;
-		*) printf "ERROR: Invalid selection"; printf $ui;;
+			if [ "$tmp" = "[" ]
+			then
+				read -rsn1 -t 0.01 tmp
+				case "$tmp" in
+					A) # ^[[A is Up key
+						echo Up
+						;;
+					B)
+						echo Down
+						;;
+					C)
+						echo Right
+						;;
+					D)
+						echo Left
+						;;
+					*)
+						;;
+				esac
+			fi
+			;;
+		"")
+			echo tree_validate > $PIPE
+			;;
+		z|w)
+			echo tree_up > $PIPE
+			;;
+		s)
+			echo tree_down > $PIPE
+			;;
+		q|a)
+			echo tree_left > $PIPE
+			;;
+		d)
+			echo tree_right > $PIPE
+			;;
+		1|" ")
+			echo play_pause > $PIPE
+			;;
+		$'\177'|2) # escape for ^? Backspace key
+			echo stop > $PIPE
+			;;
+		$'\004'|9) # escape for ^D
+			echo quit > $PIPE
+			exit 0
 	esac
-	
 done
+
+exit 0
